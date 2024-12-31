fd343b1850022f930fe68c34f370daf4e914fea7
eyoaab <eyobtariku48@gmail.com>
2024-12-31 16:28:06 +0300

    added a section to featch the token

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -11,7 +11,7 @@ const authenticateToken = (req, res, next) => {
   jwt.verify(token, process.env.JWT_SECRET, (err, id) => {
     if (err) return res.status(403).json({ message: "Invalid token" });
 
-    req.userId = id;
+    req.userId = id.id;
     next();
   });
 };
